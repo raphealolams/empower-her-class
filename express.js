c340c9a4ce3fc508faa67974df721703f3ed9a86
Ajilore Raphael Olamide <raphealolams@gmail.com>
2023-04-14 17:00:51 +0100

    simple crud

--- express.js
+++ express.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const storage = []
+
 const routes = express.Router()
 
 app.use(express.json())
@@ -11,17 +13,64 @@ routes.get("/v1", (req, res) => {
 
 const validateBody = (req, res, next) => {
   const body = req.body
-  const header = req.headers // 
-  const query = req.query
-  const params = req.params
-
   if (body && body.firstName.length === 0) {
     return res.status(400).json({message: 'First name cannot be empty'})
+  }
+  if (body && body.lastName.length === 0) {
+    return res.status(400).json({message: 'Last name cannot be empty'})
+  }
+  if (body && body.age.length === 0) {
+    return res.status(400).json({message: 'age cannot be empty'})
+  }
+  if (body && !['male', 'female'].includes(body.gender.toLowerCase())) {
+    return res.status(400).json({message: 'gender can only be male or female'})
   }
   return next()
 }
 
-routes.post("/v1", validateBody,  (req, res) => res.status(200).json({message: 'ok'}))
+routes.post("/v1/users", validateBody, (req, res) => {
+  const user = {...req.body, id: storage.length + 1}
+  storage.push(user)
+  res.status(200).json({ message: 'user created', data: user })
+})
+routes.get("/v1/users", (req, res) => {
+  res.status(200).json({message: 'users successfully retrieved', data: storage})
+})
+routes.get("/v1/users/:id", (req, res) => {
+  const userId =  req.params.id
+  const user = storage.find(item => item.id === parseInt(userId))
+  res.status(200).json({message: 'user successfully retrieved', data: user})
+})
+routes.patch("/v1/users/:id", (req, res) => {
+  const userId =  req.params.id
+  const body = req.body;
+  // storage = storage.map(item => {
+  //   if (item.id === parseInt(userId)) {
+  //     Object.keys(body).map(user => {
+  //       item[user] = body[user]
+  //     })
+  //   }
+  //   return item
+  // })
+  for (let user in storage) {
+      if (user.id === parseInt(userId)) {
+      Object.keys(body).map(item => {
+        user[item] = body[item]
+      })
+    }
+  }
+  res.status(200).json({message: 'user successfully updated', data: {}})
+})
+routes.delete("/v1/users/:id", (req, res) => {
+  const userId =  req.params.id
+  const newStorage = storage.filter((item) => item.id !== parseInt(userId))
+  storage = newStorage
+  res.status(204).json({message: 'user deleted'})
+})
+
+
+
+
 
 
 app.use(routes)
